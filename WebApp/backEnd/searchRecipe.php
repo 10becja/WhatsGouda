0d5823db2e15f1a945085308cc953a82472d4b34
Suicunee <yqian14@illinois.edu>
2015-03-31 22:45:26 -0500

    recipeSearch

--- WebApp/backEnd/searchRecipe.php
+++ WebApp/backEnd/searchRecipe.php
@@ -1,25 +1,24 @@
 <?php
-include "db.php";
-function searchRecipe($connection, $qry){
+include "base.php";
+include "../navbar.php"
+function searchRecipe($qry){
 	$sql = "SELECT * FROM Recipe WHERE name LIKE '$qry%'";
-	$result = $connection->query($sql);
-	if (!$result) { 
-		die('Invalid query: ' . mysql_error());
-	}
+	
+	$result = mysql_query($sql);
 
 	echo "<table>";
-
-	if ($result->num_rows > 0){
-	    echo "<tr><td>" . "avgQuality" . "</td><td>" . "avgDifficulty" . "</td><td>" . "reviewCount" . "</td><td>" . "name" . "</td><td>" . "creatorUsername" . "</td></tr>";
-		while($row = $result->fetch_array()){
-			echo "<tr><td>" . $row['avgQuality'] . "</td><td>" . $row['avgDifficulty'] . "</td><td>" . $row['reviewCount'] . "</td><td>" . $row['name'] . "</td><td>" . $row['creatorUsername'] . "</td></tr>";
+	
+	
+	echo "<tr><td>" . "avgQuality" . "</td><td>" . "avgDifficulty" . "</td><td>" . "reviewCount" . "</td><td>" . "name" . "</td><td>" . "creatorUsername" . "</td></tr>";
+	while($row = mysql_fetch_array($result)){
+		echo "<tr><td>" . $row['avgQuality'] . "</td><td>" . $row['avgDifficulty'] . "</td><td>" . $row['reviewCount'] . "</td><td>" . $row['name'] . "</td><td>" . $row['creatorUsername'] . "</td></tr>";
 			
-		}        
-	}
+	}        
+
 	
 }
 
-searchRecipe($conn, "pizz");
+searchRecipe($_POST["searchTerm"]);
 
 
 
