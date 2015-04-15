d6aa29cf410911f3b4de506cf289104bd0fb4689
Whats <gouda@d1stkfactory>
2015-04-15 20:23:53 +0000

    commit from server

--- WebApp/backEnd/base.php
+++ WebApp/backEnd/base.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
  
-//$dbhost = "localhost"; // used when on server
-$dbhost = "104.236.86.166";  //used for local machine testing
+$dbhost = "localhost"; // used when on server
+//$dbhost = "104.236.86.166";  //used for local machine testing
 $dbname = "gouda";
 $dbuser = "gouda";
-//$dbpass = ""; // no password from server
-$dbpass = "cs411goudapassword"; //needed for remote connections
+$dbpass = ""; // no password from server
+//$dbpass = "cs411goudapassword"; //needed for remote connections
  
 mysql_connect($dbhost, $dbuser, $dbpass) or die("MySQL Error: " . mysql_error());
 mysql_select_db($dbname) or die("MySQL Error: " . mysql_error());
