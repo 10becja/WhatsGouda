d2de69c09a979cf3d7c84e30265287db1cef3054
Thomas Nelson <tanelso2@gmail.com>
2015-04-20 20:00:42 -0500

    Fixing merge conflicts

--- WebApp/index.php
+++ WebApp/index.php
@@ -3,16 +3,17 @@
 <h1>Welcome to What's Gouda!</h1>
 <h3>Let's find out what's gooda:</h3>
 <?php
-	$query = "SELECT * FROM Recipe ORDER BY avgQuality";
+	$query = 'SELECT * FROM Recipe ORDER BY avgQuality';
 	$result = mysql_query($query);
 	$num = 0;
-	echo '<table class="table table-striped table-hover table-bordered">';
-	echo "<tr><th>" . "avgQuality" . "</th><th>" . "avgDifficulty" . "</th><th>" . "reviewCount" . "</th><th>" . "name" . "</th><th>" . "creatorUsername" . "</th></tr>";
+	//echo '<table class="table table-striped table-hover table-bordered">';
+	//echo "<tr><th>" . "avgQuality" . "</th><th>" . "avgDifficulty" . "</th><th>" . "reviewCount" . "</th><th>" . "name" . "</th><th>" . "creatorUsername" . "</th></tr>";
 	while ($row = mysql_fetch_array($result) && $num < 10) {
-		echo "<tr><td>" . $row['avgQuality'] . "</td><td>" . $row['avgDifficulty'] . "</td><td>" . $row['reviewCount'] . "</td><td><a href='viewRecipe.php?id=" . $row['id'] . "'>" . $row['name'] . "</a></td><td>" . $row['creatorUsername'] . "</td></tr>";
+		echo $row['name']
+		//echo "<tr><td>" . $row['avgQuality'] . "</td><td>" . $row['avgDifficulty'] . "</td><td>" . $row['reviewCount'] . "</td><td><a href='viewRecipe.php?id=" . $row['id'] . "'>" . $row['name'] . "</a></td><td>" . $row['creatorUsername'] . "</td></tr>";
 		$num = $num + 1;
-	}
-
+	
+}
 ?>
 
 
