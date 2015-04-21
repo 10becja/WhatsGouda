b8d1630c0ae3e9959d00e88aad517a56a409cd9b
Thomas Nelson <tanelso2@gmail.com>
2015-04-20 19:55:32 -0500

    recipe form added. time to see how it looks

--- WebApp/viewRecipe.php
+++ WebApp/viewRecipe.php
@@ -25,11 +25,60 @@ function printIngredientNames($recipeId){
 	}
 }
 
+function printReviewForm($recipeId) {
+?>
+	<form class="form-horizontal" method="post" action="" name="reviewForm" id="reviewForm">
+    <fieldset>
+<?php 
+	echo '<input type="hidden" name="recipeId" value="' . $recipeId . '"/>';
+?>
+      <div class="form-group">
+        <label class="col-lg-2 control-label">Difficulty</label>
+        <div class="col-lg-10">
+          <select class="form-control" name="difficulty" id="difficulty">
+          	<option>5</option>
+          	<option>4</option>
+          	<option>3</option>
+          	<option>2</option>
+          	<option>1</option>
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="col-lg-2 control-label">Quality</label>
+        <div class="col-lg-10">
+          <select class="form-control" name="quality" id="quality">
+          	<option>5</option>
+          	<option>4</option>
+          	<option>3</option>
+          	<option>2</option>
+          	<option>1</option>
+        </div>
+      </div>
+      <!-- Instructions -->
+      <div class="form-group">
+        <label class="col-lg-2 control-label">Review</label>
+        <div class="col-lg-10">
+          <textarea class="form-control" name="reviewBody" id="reviewBody" rows="3">
+          </textarea>
+        </div>
+      </div>
+      <!-- BUTTON -->
+      <div class="form-group">
+        <div class="col-lg-10 col-lg-offset-2">
+          <input type="submit" class="btn btn-primary" name="addRecipe" id="addRecipe" value="Submit" />
+        </div>
+      </div>
+    </fieldset>
+  </form>
+<?php	
+}
+
 	$recipeId = $_GET["id"];
 	$recipeQuery = "SELECT * FROM Recipe WHERE id=$recipeId";
 	$recipeResult = mysql_query($recipeQuery);
 	if($recipeRow = mysql_fetch_array($recipeResult)) {
 		printRecipe($recipeRow);
+		printReviewForm($recipeId);
 	}
 	else {
 		echo "<h1>Recipe not found</h1>";
