5a93c49d06f531285323c3a6ca0035a644bd3c4f
Suicunee <yqian14@illinois.edu>
2015-04-22 17:16:06 -0500

    make links to user's recipes

---
+++ WebApp/viewUserRecipe.php
@@ -0,0 +1,12 @@
+<?php include "./backEnd/base.php";?>
+<?php include "./navbar.php";?>
+<?php
+
+
+	$username = $_GET["username"];
+	$recipeQuery = "SELECT * FROM Recipe WHERE creatorUsername=$username";
+	$recipeResult = mysql_query($recipeQuery);
+	while($row = mysql_fetch_array($result)){
+		echo "<tr><td><a href='viewRecipe.php?id=" . $row['id'] . "'>" . $row['name']. "</td></tr>";
+	}  
+?>
