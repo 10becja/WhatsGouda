b23e44116dc6d0c008104b5107d9a4333819c15d
Suicunee <yqian14@illinois.edu>
2015-04-22 17:47:51 -0500

    fixing

--- WebApp/viewUserRecipe.php
+++ WebApp/viewUserRecipe.php
@@ -4,7 +4,7 @@
 
 
 	$username = $_GET["username"];
-	$recipeQuery = "SELECT * FROM Recipe WHERE creatorUsername=$username";
+	$recipeQuery = 'SELECT * FROM Recipe WHERE creatorUsername= "' . $username. '";';
 	$recipeResult = mysql_query($recipeQuery);
 	while($row = mysql_fetch_array($result)){
 		echo "<tr><td><a href='viewRecipe.php?id=" . $row['id'] . "'>" . $row['name']. "</td></tr>";
