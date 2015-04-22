3bad63a644cd4899e12a94d2c6f7ffca14bbdf36
James <10becja@gmail.com>
2015-04-22 16:26:56 -0500

    search looks better

--- WebApp/searchRecipe.php
+++ WebApp/searchRecipe.php
@@ -1,13 +1,29 @@
 <?php include "./backEnd/base.php";?>
 <?php include "./navbar.php";?>
 
+<div class="row">
+          <div class="col-lg-12">
+            <div class="page-header">
+              <h1 id="forms">Search Recipes</h1>
+            </div>
+          </div>
+        </div>
+
+                <div class="row"> 
+          <div class="col-lg-6"> 
+            <div class="well"> 
+
 <form Method="POST" action="./searchResult.php">
       <div class="form-group">
-      <label>Search for a recipe:</label>
-      <input type="text" name="searchterm" class="form-control" id="searchInput" placeholder="Enter search term here">
+      	<label>Enter a recipe to search for:</label>
+      	<input type="text" name="searchterm" class="form-control" id="searchInput" placeholder="Pizza">
       </div>
       <button type="submit" class="btn btn-default">Search</button>
       <button class="btn btn-default" onclick="./dispAllRecipes.php">Browse All</button>
     </form>
+
+    </div>
+          </div>
+        </div>
 </body>
 </html>
