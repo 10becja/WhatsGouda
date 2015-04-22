e986bc022b28501b541728cba6b25082f47dd942
Thomas Nelson <tanelso2@gmail.com>
2015-04-22 15:29:43 -0500

    Links to add and delete on controlPanel

--- WebApp/controlPanel.php
+++ WebApp/controlPanel.php
@@ -10,6 +10,7 @@
 	<div>
 	<h1>Your Recipes:</h1>
 	<!-- TODO: Add links to addRecipe and deleteRecipe here -->
+	<p>Want to make a new recipe? <a href="./addRecipe.php">Click here!</a> Want to get rid of one? <a href="./deleteRecipe.php">Click here!</a></p>
 	<table class="table table-striped table-hover table-bordered">
 	<tr>
 		<th>Quality</th>
@@ -36,6 +37,7 @@
 	$query = 'SELECT Ingredient.name AS ingredientName, Ingredient.id as ingredientID FROM Has, Ingredient WHERE Has.username="' . $username . '" AND Ingredient.id=Has.ingredientID';
 	$result = mysql_query($query);
 	//TODO: Add links to addIngredient and deleteIngredient here
+	echo "<p>Got a new ingredient? <a href='./addIngredient.php'>Click here to add it!</a> Don't have an ingredient anymore? <a href='./deleteIngredient.php'>Click here to delete some!</a></p>";
 	echo "<div>";
 	echo "<h1>Your Ingredients:</h1>";
 	echo "<ul>";
